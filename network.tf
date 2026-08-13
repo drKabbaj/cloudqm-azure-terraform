e1923d18809e42b91d468f90bd5ba72857f87662
Fatima Zahra Kabbaj <dr.kabbaj.fz@gmail.com>
2026-08-13 13:51:21 +0200

    Add database subnet

--- network.tf
+++ network.tf
@@ -12,6 +12,13 @@ resource "azurerm_subnet" "web" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_subnet" "database" {
+  name                 = "database-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 resource "azurerm_network_security_group" "web" {
   name                = "nsg-web-dev"
   location            = azurerm_resource_group.main.location
